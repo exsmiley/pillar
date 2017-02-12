3c226652486466ef48b38bee29d9b8e07b802d80
Chan Pham <tqp2001@columbia.edu>
2017-02-12 05:50:05 -0500

    pulling data for dashboard (incomplete)

--- app/static/script.js
+++ app/static/script.js
@@ -61,55 +61,21 @@ var app4 = new Vue({
 })
 
 $(document).ready(function(){
-// var categories=["  Agriculture and Food",
-// "Animals",
-// "Armed Forces and National Security",
-// "Arts, Culture, Religion",
-// "Civil Rights and Liberties, Minority Issues",
-// "Commerce",
-// "Congress",
-// "Crime and Law Enforcement",
-// "Economics and Public Finance",
-// "Education",
-// "Emergency Management",
-// "Energy",
-// "Environmental Protection",
-// "Families",
-// "Finance and Financial Sector",
-// "Foreign Trade and International Finance",
-// "Government Operations and Politics",
-// "Health",
-// "Housing and Community Development",
-// "Immigration",
-// "International Affairs",
-// "Labor and Employment",
-// "Law",
-// "Native Americans",
-// "Public Lands and Natural Resources",
-// "Science, Technology, Communications",
-// "Social Sciences and History",
-// "Social Welfare",
-// "Sports and Recreation",
-// "Taxation",
-// "Transportation and Public Works",
-// "Water Resources Development",
-// ]
-var categories = ['Ways and Means', 'Oversight and Government Reform', 'Natural Resources', 'Energy and Natural Resources', 'Indian Affairs', 'Small Business and Entrepreneurship', 'Transportation and Infrastructure', 'Administration', 'Judiciary', 'Homeland Security and Governmental Affairs', 'Energy and Commerce', 'Health, Education, Labor, and Pensions', 'Banking, Housing, and Urban Affairs', "Veterans' Affairs", 'Commerce, Science, and Transportation', 'Education and the Workforce', 'Armed Services', 'Agriculture', 'Foreign Relations', 'Finance']
+    var categories = ['Ways and Means', 'Oversight and Government Reform', 'Natural Resources', 'Energy and Natural Resources', 'Indian Affairs', 'Small Business and Entrepreneurship', 'Transportation and Infrastructure', 'Administration', 'Judiciary', 'Homeland Security and Governmental Affairs', 'Energy and Commerce', 'Health, Education, Labor, and Pensions', 'Banking, Housing, and Urban Affairs', "Veterans' Affairs", 'Commerce, Science, and Transportation', 'Education and the Workforce', 'Armed Services', 'Agriculture', 'Foreign Relations', 'Finance']
+    $.each(categories,function(index,value){
+        var checkbox="<input type='checkbox' class='x' value="+value+" name="+value+" style='padding-left: 10px; padding-right: 10px;'><label for="+value+">"+value+"</label> <br>"
+        $(".checkBoxContainer").append($(checkbox));
+    })
+}
 
-
-$.each(categories,function(index,value){
-    var checkbox="<input type='checkbox' class='x' value="+value+" name="+value+" style='padding-left: 10px; padding-right: 10px;'><label for="+value+">"+value+"</label> <br>"
-    $(".checkBoxContainer").append($(checkbox));
-})
-
-
+/*
 $(function() {
     $('button').click(function() {
         var user = $('#txtUsername').val();
         var pass = $('#txtPassword').val();
         var confirmPass = $('#txtconfirmPassword').val();
         var state = $('#txtState').val();
-/*        $.ajax({
+        $.ajax({
             url: '/signUp',
             data: $('form').serialize(),
             type: 'POST',
@@ -119,7 +85,7 @@ $(function() {
             error: function(error) {
                 console.log(error);
             }
-        });*/
+        });
 
         var dataList = document.getElementById('state');
         var input = document.getElementById('ajax');
@@ -134,4 +100,4 @@ $(function() {
             });
         });
     });
-});
+});*/
