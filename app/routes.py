070580f5e67cfe2e7ad4d91414954748e6242684
Zachery Miranda <zmiranda@mit.edu>
2017-02-11 21:34:44 -0500

    force browser update no cache

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,19 @@ from flask import Flask, render_template, request, jsonify, make_response
 from bills import *
 from dbagent import *
 
-app = Flask(__name__, static_url_path='/static')      
+app = Flask(__name__)      
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route('/')
 def home():
