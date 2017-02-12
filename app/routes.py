a40931ee9851f7b4975bdf5f2ce93eec91ac9096
Zachery Miranda <zmiranda@mit.edu>
2017-02-12 04:22:11 -0500

    try again deploy with port environ var

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, make_response, session, redirect, url_for
 import hashlib
+import os
 from bills import *
 from dbagent import *
 
@@ -97,4 +98,6 @@ def tester():
   
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
